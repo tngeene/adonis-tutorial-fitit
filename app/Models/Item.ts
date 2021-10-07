f22e7a58dfb9911903618ad88217d1af398cec69
tngeene <tngeene27@gmail.com>
2021-10-07 18:29:52 +0300

    added relationship definitions

--- app/Models/Item.ts
+++ app/Models/Item.ts
@@ -1,5 +1,7 @@
+import { BaseModel, column, hasOne, HasOne } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import Store from './Store'
+import User from './User'
 
 export default class Item extends BaseModel {
   @column({ isPrimary: true })
@@ -16,6 +18,20 @@ export default class Item extends BaseModel {
 
   @column()
   public price: number
+
+  // can only be uploaed by one person i.e user who created it.
+  @hasOne(() => User, {
+    localKey: 'uploaded_by',
+    foreignKey: 'id',
+  })
+  public owner: HasOne<typeof User>
+
+  // an item belongs to a single store
+  @hasOne(() => Store, {
+    localKey: 'store',
+    foreignKey: 'id',
+  })
+  public store: HasOne<typeof Store>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
--- app/Models/Store.ts
+++ app/Models/Store.ts
@@ -1,5 +1,6 @@
+import { BaseModel, column, hasOne, HasOne, manyToMany, ManyToMany } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
 
 export default class Store extends BaseModel {
   @column({ isPrimary: true })
@@ -22,6 +23,23 @@ export default class Store extends BaseModel {
 
   @column()
   public isActive: boolean
+
+  // can only have one owner i.e user who created it.
+  @hasOne(() => User, {
+    localKey: 'owner',
+    foreignKey: 'id',
+  })
+  public owner: HasOne<typeof User>
+
+  // a store has many followers
+  @manyToMany(() => User, {
+    pivotTable: 'store_followers',
+    pivotForeignKey: 'store_id',
+    pivotRelatedForeignKey: 'user_id',
+    localKey: 'id',
+    relatedKey: 'id',
+  })
+  public followers: ManyToMany<typeof User>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
